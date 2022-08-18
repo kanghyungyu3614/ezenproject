f5d58ada86add79d098beedbc8cd40323eeef31a
504 <504@DESKTOP-1OTV5S3>
2022-08-18 09:17:07 +0900

    엘리베이터 완성?

--- front/public/Elevator/Elevator.js
+++ front/public/Elevator/Elevator.js
@@ -16,11 +16,17 @@ document.getElementById("State_Mine").innerHTML = `내 층수 :  ${ElevatorMine}
 function up_button (){
 	ElevatorFloor += 1;
 	document.getElementById("State_Elevator").innerHTML = `엘리베이터 층수 : ${ElevatorFloor}`
+	//엘리베이터가 15층보다 높을때 
 	if(ElevatorFloor>15){
 	alert("더이상 움직일수 없습니다.")
 	ElevatorFloor -= 1;
 	document.getElementById("State_Elevator").innerHTML = `엘리베이터 층수 : ${ElevatorFloor}`
-}
+    }
+    //1층에 도착했을때 
+    if(ElevatorFloor===1){
+	document.getElementById("State_Elevator").innerHTML = `1층에 도착했습니다.`
+	}
+
 }
 
 
@@ -29,9 +35,20 @@ function up_button (){
 function down_button (){
 	ElevatorFloor -= 1;
 	document.getElementById("State_Elevator").innerHTML = `엘리베이터 층수 : ${ElevatorFloor}`
-	if(ElevatorFloor<0){
+	//엘리베이터가 0층보다 낮을때 
+	if(ElevatorFloor===1){
+	document.getElementById("State_Elevator").innerHTML = `1층에 도착했습니다.`
+	}
+	if(ElevatorFloor<0){ 
 	alert("더이상 움직일수 없습니다.")
 	ElevatorFloor += 1;
 	document.getElementById("State_Elevator").innerHTML = `엘리베이터 층수 : ${ElevatorFloor}`
 }
+
+if(ElevatorFloor === ElevatorMine){
+	alert("엘리베이터가 도착했습니다.")
+	document.getElementById("State_Mine").innerHTML = "엘리베이터가 도착했습니다."
+}
+
+
 }
