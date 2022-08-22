55c7b42e4ed4444e5247716d6a59f2898bb18b48
504 <504@DESKTOP-1OTV5S3>
2022-08-22 17:58:06 +0900

    학생관리 미완성

---
+++ front/public/JS/객체/4.객체.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+ const object = {
+	name: "혼자 공부하는 파이썬",
+	price: 18000,
+	publisher : "한빛미디어"
+}
---
+++ front/public/JS/객체/5.변수관리.js
@@ -0,0 +1,122 @@
+/**
+     * 학생 점수 프로그램
+       1. 학생 객체 설계
+       2. HTML에서 입력받은 데이터를 JS객체에 저장
+      
+ */
+ let studentlist = []
+ 학생출력()
+ 
+ 
+ function 점수등록(){
+	
+	 const student ={
+		name : document.getElementById("name").value,
+		kor : Number(document.getElementById("kor").value),
+		eng : Number(document.getElementById("eng").value),
+		math : Number(document.getElementById("math").value) 
+	}
+	
+	if(이름중복체크(student.name)){
+		return 
+	}
+	
+	studentlist.push(student)
+	
+	alert("학생 점수 등록이 되었습니다.")
+	console.log("studentlist",studentlist)
+	
+	document.getElementById("name").value = ""
+	document.getElementById("kor").value = ""
+	document.getElementById("eng").value = ""
+	document.getElementById("math").value = ""
+	
+	학생출력()
+}
+
+
+function 이름중복체크(name){
+	for(let i=0; i<studentlist.length; i++){
+	if(studentlist[i].name === name){
+		alert("이미 존재한 학생명입니다.")
+		return true;
+	}
+}
+    return false;
+}
+
+function 학생출력(){
+
+	let html ="<tr>" +
+	                "<th>등수</th>" +
+	                "<th>학생명</th>" +
+	                "<th>국어</th>" +
+	                "<th>영어</th>" +
+	                "<th>수학</th>" +
+	                "<th>총점</th>" +
+	                "<th>평균</th>" +
+	                "<th>비고</th>" +
+	          "</tr>"
+	          
+	for(let i=0; i<studentlist.length; i++){
+		let sum =studentlist[i].kor + studentlist[i].eng+studentlist[i].math
+		let avg = sum/3
+		학생랭킹()
+		console.log(studentlist)
+		// i는 0부터 배열내 마지막인덱스까지[배열의 길이-1] 1씩 증가반복
+		html += "<tr>" +
+	                "<th>"+i+"</th>" +
+	                "<th>"+studentlist[i].name+"</th>" +
+	                "<th>"+studentlist[i].kor+"</th>" +
+	                "<th>"+studentlist[i].eng+"</th>" +
+	                "<th>"+studentlist[i].math+"</th>" +
+	                "<th>"+sum+ "</th>" +
+	                "<th>"+avg+"</th>" +
+	                "<th><button onclick='학생삭제("+i+")'>삭제</button></th>" +
+	          "</tr>"
+		 
+	}          
+	
+	   
+	          
+    document.getElementById("listtable").innerHTML = html
+
+}
+function 학생삭제(i){
+	studentlist.splice(i,1)
+	학생출력()
+}
+
+function 학생랭킹(){
+	let len = studentlist.length;
+	let min = "";
+
+    for(let i=0; i<len; i++){
+       for(let j=0; j<len; j++){
+		let isum =Number(studentlist[i].kor) + Number(studentlist[i].eng)+Number(studentlist[i].math)
+		let iavg = isum/3
+		let jsum =studentlist[j].kor + studentlist[j].eng+studentlist[j].math
+		let javg = jsum/3
+	    if(iavg < javg){
+		min = studentlist.splice(i,1)[0]
+		studentlist[j] = min
+		
+	 }else{ continue;}   
+     }	
+   }
+   return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
